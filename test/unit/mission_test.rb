5eba4a4904875763f58c0470dd93204d332d95ff
philomory <philomory@localhost>
2010-02-10 14:12:59 -1000

    Add tests for missions.

--- test/unit/mission_test.rb
+++ test/unit/mission_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class MissionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "a new mission should be in_progress" do
+    assert Mission.make.in_progress?
   end
+  
+  test "a mission tick should adjust progress" do
+    mission = Mission.make
+    assert_different(mission.progress) do
+      mission.tick
+    end
+  end
+  
 end
