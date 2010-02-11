416724928bd9e43feeaedf5db676c19562ce05eb
philomory <philomory@localhost>
2010-02-11 13:05:02 -1000

    fixed auto-generated missions_controller_test.rb to work without fixtures.

--- test/functional/missions_controller_test.rb
+++ test/functional/missions_controller_test.rb
@@ -13,31 +13,33 @@ class MissionsControllerTest < ActionController::TestCase
   end
 
   test "should create mission" do
+    mission = Mission.plan
     assert_difference('Mission.count') do
-      post :create, :mission => { }
+      post :create, :mission => mission
     end
 
     assert_redirected_to mission_path(assigns(:mission))
   end
 
   test "should show mission" do
-    get :show, :id => missions(:one).to_param
+    get :show, :id => Mission.make.to_param
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => missions(:one).to_param
+    get :edit, :id => Mission.make.to_param
     assert_response :success
   end
 
   test "should update mission" do
-    put :update, :id => missions(:one).to_param, :mission => { }
+    put :update, :id => Mission.make.to_param, :mission => { }
     assert_redirected_to mission_path(assigns(:mission))
   end
 
   test "should destroy mission" do
+    mission = Mission.make
     assert_difference('Mission.count', -1) do
-      delete :destroy, :id => missions(:one).to_param
+      delete :destroy, :id => mission.to_param
     end
 
     assert_redirected_to missions_path
